f6f1bacdfd5b90fef05a294e2aac80d5f1069c91
Wishva98 <buddhiwishvanath123@gmail.com>
2023-12-01 01:04:31 +0530

    set up spring configuration

---
+++ back-end/src/main/java/lk/ijse/dep11/web/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep11.web;
+
+import lk.ijse.dep11.web.controller.ChatWSController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWSController(),"/api/v1").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ChatWSController chatWSController(){
+        return new ChatWSController();
+    }
+}
---
+++ back-end/src/main/java/lk/ijse/dep11/web/controller/ChatWSController.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+public class ChatWSController extends TextWebSocketHandler {
+
+}
